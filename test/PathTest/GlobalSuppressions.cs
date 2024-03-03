9a8e2a2f93f5c7080af3ed6c74319276410b23cd
Jason Curl <jcurl@arcor.de>
2024-03-03 12:29:55 +0100

    Path: Suppress IDE0230 for UTF-8 string literals
    The test cases use bytes, that just so happens to be the same as a string literal. But the test case should use the bytes, as that is the underlying interface.
    
    Issue: DOTNET-995

--- test/PathTest/GlobalSuppressions.cs
+++ test/PathTest/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0056:Use index operator", Justification = "NET Framework code")]
+[assembly: SuppressMessage("Style", "IDE0230:Use UTF-8 string literal", Justification = "<Pending>")]
